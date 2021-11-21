cb9d2f342986f1c1b94d1f8cb6760877cd0e88fe
Saifoo Khan <isaifookhan@gmail.com>
2021-11-21 23:49:22 +0100

    clean create_react_app

---
+++ src/ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Row from "./components/Row";
+
+function App() {
+  return (
+    <div className="App">
+      <Row />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ui/src/components/Row.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Row = () => {
+  const [trainPosition, setTrainPosition] = useState(0);
+
+  return <div>_</div>;
+};
+
+export default Row;

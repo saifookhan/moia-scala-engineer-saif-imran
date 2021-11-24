0ebcf32b35261a6c9896a8a0be4efee3e353a069
Saifoo Khan <isaifookhan@gmail.com>
2021-11-24 11:38:12 +0100

    config updated + dynamic reouting

--- src/ui/src/constants/config.js
+++ src/ui/src/constants/config.js
@@ -1,2 +1,5 @@
-const config = { serverUrl: "http://localhost:5000/api/" };
+const config = {
+  serverUrl: "http://localhost:5000/api/",
+  prodUrl: "https://moia-challenge-be.herokuapp.com/api/",
+};
 export default config;
--- src/ui/src/helpers/serverModule/serverModule.ts
+++ src/ui/src/helpers/serverModule/serverModule.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import config from "../../constants/config";
 
+const workingUrl =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? config.serverUrl
+    : config.prodUrl;
+
 export const getDashboardStatus = async () => {
   try {
-    var res = await axios.get<any>(`${config.serverUrl}dashboard`);
+    var res = await axios.get<any>(`${workingUrl}dashboard`);
     console.log(res);
     return res;
   } catch (e) {
@@ -14,7 +19,7 @@ export const getDashboardStatus = async () => {
 
 export const requestATrain = async (from: number, to: number) => {
   try {
-    var res = await axios.post<any>(`${config.serverUrl}train/request`, {
+    var res = await axios.post<any>(`${workingUrl}train/request`, {
       startTerminal: from,
       endTerminal: to,
       requestedTime: 0,
